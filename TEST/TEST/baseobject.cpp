80078de9db2468004f939031ee3e5ea989522110
Nguyenvaan77 <anvan16022005@gmail.com>
2024-04-12 00:55:01 +0700

    Du lieu co so

---
+++ TEST/TEST/baseobject.cpp
@@ -0,0 +1,52 @@
+#include"commonFunc.h"
+#include"baseobject.h"
+
+baseObject::baseObject()
+{
+	object_ = NULL;
+	rect_.x = 0;
+	rect_.y = 0;
+	rect_.w = 0;
+	rect_.h = 0;
+};
+
+baseObject::~baseObject()
+{
+	
+}
+
+bool baseObject::loadImg(std::string path,SDL_Renderer* ren)
+{
+	bool loadOk = true;
+	SDL_Texture* new_text = NULL;
+	SDL_Surface* sur = NULL;
+	sur = SDL_LoadBMP(path.c_str());
+	if (sur == NULL)
+	{
+		std::cout << "sur == NULL" << std::endl;
+		loadOk = false;
+		return loadOk;
+	}
+	else
+	{
+		rect_.w = sur->w;
+		rect_.h = sur->h;
+		SDL_SetColorKey(sur, SDL_TRUE, SDL_MapRGB(sur->format, 12, 34, 56));
+		new_text = SDL_CreateTextureFromSurface(ren, sur);
+		object_ = new_text;
+		SDL_FreeSurface(sur);
+	}
+	if (new_text == NULL)
+	{
+		std::cout << "ERROR NEW TEXT=NULL" << std::endl;
+		loadOk = false;
+		return loadOk;
+	}
+	return loadOk;
+};
+
+void baseObject::render(SDL_Renderer* ren,SDL_Rect* rec)
+{
+	SDL_RenderCopy(ren, object_, NULL, rec);
+}
+
---
+++ TEST/TEST/baseobject.h
@@ -0,0 +1,21 @@
+#ifndef BASE_OBJECT_H_
+#define BASE_OBJECT_H_
+
+#include"commonFunc.h"
+
+class baseObject
+{
+public:
+	baseObject();
+	~baseObject();
+	void setRect(const int& a, const int& b) { this->rect_.x = a; this->rect_.y = b;  };
+	SDL_Texture* getObject() { return object_; };
+	bool loadImg(std::string path,SDL_Renderer* ren);
+	void render(SDL_Renderer* ren,SDL_Rect* rec);
+
+	SDL_Texture* object_;
+	SDL_Rect     rect_;
+};
+
+
+#endif // !BASE_OBJECT_H_
